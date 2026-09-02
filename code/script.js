3dc3e08ab3bf6b1429be1a124c198a72bc38bea5
Biscutbuilder <nicklogan1412@gmail.com>
2026-09-01 20:03:33 -0500

    Add function to update time every second
    Fix innerHTML property and add time update function.

--- code/script.js
+++ code/script.js
@@ -1,1 +1,6 @@
-
+function updateTime(){
+    var currentTime = new Date().toLocaleString();
+var timeText = document.querySelector("#timeElement");
+timeText.innerhtml = currentTime;
+}
+setInterval(updateTime, 1000);
